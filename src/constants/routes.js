4d97374060aeb2983f01ca564e3fa96995d74630
Ahrar Monsur <ahrarmonsur93@gmail.com>
2019-08-24 17:02:49 -0400

    Adds folder structure to project, and replaces css with sass

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const HOME = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const ACCOUNT = '/account';
+export const FORGOT_PASSWORD = '/forgot-pw';
+export const PROFILE_SETUP = '/profile-setup';
